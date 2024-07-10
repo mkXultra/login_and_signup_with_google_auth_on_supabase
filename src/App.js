e99252e7e65e64ceb4a475d1f6bd2592c2fce6de
mkXultra <electrincgoat@gmail.com>
2024-07-11 08:32:26 +0900

    imeplemet sign out

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React  from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
 function App() {
+  const [session, setSession] = useState(null)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) console.log('Error signing out:', error.message)
+  }
   return (
     <div className="App">
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={['google']}
-      />
+      {
+        session ? (
+          <div>
+            <h1>Welcome, {session.user.role}</h1>
+             <button onClick={handleSignOut}>Sign Out</button>
+          </div>
+        ) : (
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={['google']}
+            redirectTo="http://localhost:3000"
+          />
+        )
+      }
     </div>
   );
 }
