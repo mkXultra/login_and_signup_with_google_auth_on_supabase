93d24cdb8c4eb79eb8d7e38e5808838c988a7d35
mkXultra <electrincgoat@gmail.com>
2024-07-11 08:36:05 +0900

    stage support

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 function App() {
   const [session, setSession] = useState(null)
+
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
@@ -24,21 +26,30 @@ function App() {
     const { error } = await supabase.auth.signOut()
     if (error) console.log('Error signing out:', error.message)
   }
+
+  const authProps = {
+    supabaseClient: supabase,
+    appearance: { theme: ThemeSupa },
+    providers: ['google'],
+  }
+
+  // Only add redirectTo for local development
+  if (process.env.NODE_ENV === 'development') {
+    authProps.redirectTo = "http://localhost:3000"
+  }else{
+    authProps.redirectTo = "https://login-and-signup-with-google-auth-on-supabase.vercel.app"
+  }
+
   return (
     <div className="App">
       {
         session ? (
           <div>
             <h1>Welcome, {session.user.role}</h1>
-             <button onClick={handleSignOut}>Sign Out</button>
+            <button onClick={handleSignOut}>Sign Out</button>
           </div>
         ) : (
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={['google']}
-            redirectTo="http://localhost:3000"
-          />
+          <Auth {...authProps} />
         )
       }
     </div>
